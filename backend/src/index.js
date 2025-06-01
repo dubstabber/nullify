85dad3a0f43eaff7e4a03dfa775b0821c874b495
dubstabber <taesan@protonmail.com>
2025-06-01 16:46:51 +0200

    Setup mongo database,signup/login logic, structure code with folders

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,16 +1,20 @@
 import express from "express";
 import dotenv from "dotenv";
+
 import userRoutes from "./routes/user.route.js";
 import adminRoutes from "./routes/admin.route.js";
 import authRoutes from "./routes/auth.route.js";
 import songRoutes from "./routes/song.route.js";
 import albumRoutes from "./routes/album.route.js";
 import statRoutes from "./routes/stat.route.js";
+import { connectDB } from "./lib/db.js";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
 
 app.use("/api/users", userRoutes);
 app.use("/api/admin", adminRoutes);
@@ -21,4 +25,5 @@ app.use("/api/stats", statRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
+  connectDB();
 });
