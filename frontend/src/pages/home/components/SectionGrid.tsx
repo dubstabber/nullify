045e1e35e508976bd03124ced6ecd947fa001826
dubstabber <taesan@protonmail.com>
2025-06-06 19:55:57 +0200

    Create play button component and handle song playing

--- frontend/src/pages/home/components/SectionGrid.tsx
+++ frontend/src/pages/home/components/SectionGrid.tsx
@@ -1,6 +1,7 @@
 import SectionGridSkeleton from "@/components/skeletons/SectionGridSkeleton";
 import { Button } from "@/components/ui/button";
 import type { Song } from "@/types";
+import PlayButton from "./PlayButton";
 
 type SectionGridProps = {
   title: string;
@@ -34,7 +35,7 @@ const SectionGrid = ({ title, songs, isLoading }: SectionGridProps) => {
                   alt={song.title}
                   className="w-full h-full object-cover transition-transform duration-300 group-hover:scale-105"
                 />
-                {/* TODO: Add play button */}
+                <PlayButton song={song} />
               </div>
             </div>
             <h3 className="font-medium mb-2 truncate">{song.title}</h3>
