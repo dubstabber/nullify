0708a2e341c0d88cf3b57dee8e50528d829dbac2
dubstabber <taesan@protonmail.com>
2025-06-09 21:07:27 +0200

    Handle creating albums in admin page, create modal and uploading albums

--- frontend/src/pages/admin/components/AlbumsTabContent.tsx
+++ frontend/src/pages/admin/components/AlbumsTabContent.tsx
@@ -1,5 +1,35 @@
+import { Library } from "lucide-react";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import AlbumsTable from "./AlbumsTable";
+import AddAlbumDialog from "./AddAlbumDialog";
+
 const AlbumsTabContent = () => {
-  return <div>AlbumsTabContent</div>;
+  return (
+    <Card className="bg-zinc-800/50 border-zinc-700/50">
+      <CardHeader>
+        <div className="flex items-center justify-between">
+          <div>
+            <CardTitle className="flex items-center gap-2">
+              <Library />
+              Albums Library
+            </CardTitle>
+            <CardDescription>Manage your album collection</CardDescription>
+          </div>
+          <AddAlbumDialog />
+        </div>
+      </CardHeader>
+
+      <CardContent>
+        <AlbumsTable />
+      </CardContent>
+    </Card>
+  );
 };
 
 export default AlbumsTabContent;
