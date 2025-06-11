3f60a3accee67fec8e33cadaa2e7c229aadbda15
dubstabber <taesan@protonmail.com>
2025-06-11 21:30:03 +0200

    Implement socket.io for realtime features

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,6 +4,7 @@ import { clerkMiddleware } from "@clerk/express";
 import fileUpload from "express-fileupload";
 import path from "path";
 import cors from "cors";
+import { createServer } from "http";
 
 import userRoutes from "./routes/user.route.js";
 import adminRoutes from "./routes/admin.route.js";
@@ -13,11 +14,16 @@ import albumRoutes from "./routes/album.route.js";
 import statRoutes from "./routes/stat.route.js";
 import { connectDB } from "./lib/db.js";
 
+import { initializeSocket } from "./lib/socket.js";
+
 dotenv.config();
 
 const __dirname = path.resolve();
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+const httpServer = createServer(app);
+initializeSocket(httpServer);
 
 app.use(cors({ origin: "http://localhost:3000", credentials: true }));
 
@@ -52,7 +58,7 @@ app.use((err, req, res, next) => {
   });
 });
 
-app.listen(PORT, () => {
+httpServer.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
   connectDB();
 });
--- frontend/src/providers/AuthProvider.tsx
+++ frontend/src/providers/AuthProvider.tsx
@@ -1,5 +1,6 @@
 import { axiosInstance } from "@/lib/axios";
 import { useAuthStore } from "@/stores/useAuthStore";
+import { useChatStore } from "@/stores/useChatStore";
 import { useAuth } from "@clerk/clerk-react";
 import { Loader } from "lucide-react";
 import { useEffect, useState } from "react";
@@ -11,9 +12,10 @@ const updateApiToken = (token: string | null) => {
 };
 
 const AuthProvider = ({ children }: { children: React.ReactNode }) => {
-  const { getToken } = useAuth();
+  const { getToken, userId } = useAuth();
   const [loading, setLoading] = useState(true);
   const { checkAdminStatus } = useAuthStore();
+  const { initSocket, disconnectSocket } = useChatStore();
 
   useEffect(() => {
     const initAuth = async () => {
@@ -22,6 +24,7 @@ const AuthProvider = ({ children }: { children: React.ReactNode }) => {
         updateApiToken(token || "");
         if (token) {
           await checkAdminStatus();
+          if (userId) initSocket(userId);
         }
       } catch (error) {
         updateApiToken(null);
@@ -32,7 +35,9 @@ const AuthProvider = ({ children }: { children: React.ReactNode }) => {
     };
 
     initAuth();
-  }, [getToken, checkAdminStatus]);
+
+    return () => disconnectSocket();
+  }, [getToken, checkAdminStatus, initSocket, disconnectSocket, userId]);
 
   if (loading)
     return (
